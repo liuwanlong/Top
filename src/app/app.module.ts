794736ba406b47c0d750b4c1fa9215aa62e6c36d
cy <920281921@qq.com>
2017-07-18 13:52:45 +0800

    静态完成：mine + login + register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
 
 import {NewsPage} from '../pages/news/news';
-import { VideoPage } from '../pages/video/video';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {VideoPage} from '../pages/video/video';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 import {MinePage} from "../pages/mine/mine";
+import {RegisterPage} from "../pages/register/register";
+import {LoginPage} from "../pages/login/login";
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import {MinePage} from "../pages/mine/mine";
     VideoPage,
     HomePage,
     TabsPage,
-    MinePage
+    MinePage,
+    RegisterPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +36,9 @@ import {MinePage} from "../pages/mine/mine";
     VideoPage,
     HomePage,
     TabsPage,
-    MinePage
+    MinePage,
+    RegisterPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
@@ -40,4 +46,5 @@ import {MinePage} from "../pages/mine/mine";
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/mine/mine.ts
+++ src/pages/mine/mine.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the MinePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {RegisterPage} from "../register/register";
+import {LoginPage} from "../login/login";
 
 @Component({
   selector: 'page-mine',
   templateUrl: 'mine.html',
 })
 export class MinePage {
+  items = [
+    {icon: "star", name: "我的收藏", nav: "CollectionPage"},
+    {icon: "text", name: "消息通知", nav: "NotifyPage"},
+    {icon: "settings", name: "设置", nav: "MinePage"},
+  ]
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -20,5 +20,18 @@ export class MinePage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad MinePage');
   }
+
+
+  toRegisterPage(param?: Object) {
+    this.navCtrl.push(RegisterPage, param);
+  }
+
+  toLoginPage(param?: Object) {
+    this.navCtrl.push(LoginPage, param);
+  }
+
+  // back(){
+  //   this.navCtrl.pop();
+  // }
 
 }
