d9f8ad4fd333dc11296fcbf6990cac2ed616bf8c
liuwanlong <flyinglong@outlook.com>
2017-07-20 14:47:11 +0800

    增加 弹出评论 对话框

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {AlertController, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the DetailPage page.
@@ -15,13 +15,43 @@ import {NavController, NavParams} from 'ionic-angular';
 export class DetailPage {
   slide: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public alertCtrl: AlertController) {
     this.slide = navParams.data;
   }
 
   ionViewDidLoad() {
     //当页面加载的时候
     // console.log('ionViewDidLoad DetailPage');
+  }
+
+  doPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: '评论',
+      message: "说点什么吧....",
+      inputs: [
+        {
+          name: 'topic-cont',
+          type: 'text'
+        },
+      ],
+      buttons:[
+        {
+          text:'取消',
+          handler:data=>{
+            console.log('点击了取消');
+          }
+        },
+        {
+          text:'发送',
+          handler:data=>{
+            console.log('点击了发送');
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
