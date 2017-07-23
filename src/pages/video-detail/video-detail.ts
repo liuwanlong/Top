adbabc9ab718c8197562928d14ea40648a353eee
cy <920281921@qq.com>
2017-07-23 20:50:45 +0800

    部分样式

--- src/pages/video-detail/video-detail.ts
+++ src/pages/video-detail/video-detail.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 import {DomSanitizer} from "@angular/platform-browser";
 
 /**
@@ -10,34 +10,32 @@ import {DomSanitizer} from "@angular/platform-browser";
  */
 
 @Component({
-  selector: 'page-video-detail',
-  templateUrl: 'video-detail.html',
+    selector: 'page-video-detail',
+    templateUrl: 'video-detail.html',
 })
 
-
-  export class VideoDetailPage {
-    videoUrl:any;
+export class VideoDetailPage {
+    videoUrl: any;
     news = [];
     newsArr = [];
     videoArr = [];
-    constructor(
-      public navCtrl: NavController,
-      public navParams: NavParams,
-      private sanitizer:DomSanitizer
-  ) {
-    this.news = navParams.data[0];
-    this.newsArr = navParams.data[1];
-    this.videoArr = this.newsArr.slice(1,6);
-    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(navParams.data[0].url);
-  }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VideoDetailPage');
-  }
-  videoDetailPage(news:object,newsArr?:object){
-    console.log(newsArr);
-    var data = [news,newsArr];
-    this.navCtrl.push(VideoDetailPage,data);
-  }
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                private sanitizer: DomSanitizer) {
+        this.news = navParams.data[0];
+        this.newsArr = navParams.data[1];
+        this.videoArr = this.newsArr.slice(1, 6);
+        this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(navParams.data[0].url);
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad VideoDetailPage');
+    }
+
+    videoDetailPage(news: object, newsArr?: object) {
+        var data = [news, newsArr];
+        this.navCtrl.push(VideoDetailPage, data);
+    }
 
 }
