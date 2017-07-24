f44f79ddd43dceec2dc6199cd3948bd4e7ea2f96
cy <920281921@qq.com>
2017-07-24 12:27:09 +0800

    v1.1

--- src/pages/video/video.ts
+++ src/pages/video/video.ts
@@ -1,69 +1,67 @@
 import {Component, OnInit} from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
 import {Http} from "@angular/http";
 import {VideoDetailPage} from "../video-detail/video-detail";
 import {SearchPage} from "../search/search";
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'video.html'
+    selector: 'page-contact',
+    templateUrl: 'video.html'
 })
-export class VideoPage implements OnInit{
+export class VideoPage implements OnInit {
 
-  newsArr = [];
-  number = 5;
-  isHasMore:boolean;
+    newsArr = [];
+    number = 5;
+    isHasMore: boolean;
 
-  constructor(
-    public navCtrl: NavController,
-    public http:Http,
-  ) {}
+    constructor(public navCtrl: NavController,
+                public http: Http,) {
+    }
 
-  ngOnInit(): void {
-    this.getData();
-  }
+    ngOnInit(): void {
+        this.getData();
+    }
 
-  getData(){
-    this.number += 5;
-    return this. http.get('http://localhost:3000/video/'+ this.number)
-        .toPromise()
-        .then(res=>{
+    getData() {
+        this.number += 5;
+        return this.http.get('http://localhost:3000/video/' + this.number)
+            .toPromise()
+            .then(res => {
+                this.newsArr = res.json();
+            })
+    }
 
-          this.newsArr = res.json();
-          // console.log(this.newsArr)
-        })
-  }
+    videoDetailPage(news: object, newsArr?: object) {
+        var data = [news, newsArr];
+        this.navCtrl.push(VideoDetailPage, data);
+    }
 
-  videoDetailPage(news:object,newsArr?:object){
-    var data = [news,newsArr];
-    this.navCtrl.push(VideoDetailPage,data);
-  }
-  search(){
-    this.navCtrl.push(SearchPage);
-  }
-  doRefresh(refresher) {
-    this.getData();
+    search() {
+        this.navCtrl.push(SearchPage);
+    }
 
-    setTimeout(() => {
-      console.log('Async operation has ended');
-      refresher.complete();
-    }, 2000);
+    doRefresh(refresher) {
+        this.getData();
 
-  }
-  doInfinite(infiniteScroll) {
-      this.number += 5;
-      // console.log(this.number);
-    setTimeout(() => {
-      if(this.number == 35){
-        infiniteScroll.complete();
-        this.isHasMore = true;
-        return;
-      }
+        setTimeout(() => {
+            console.log('Async operation has ended');
+            refresher.complete();
+        }, 2000);
 
-      this.getData();
-      console.log('Async operation has ended');
-      infiniteScroll.complete();
-    }, 500);
-  }
+    }
+
+    doInfinite(infiniteScroll) {
+        this.number += 5;
+        setTimeout(() => {
+            if (this.number >= 60) {
+                infiniteScroll.complete();
+                this.isHasMore = true;
+                return;
+            }
+            this.getData();
+            console.log('Async operation has ended');
+            infiniteScroll.complete();
+        }, 500);
+    }
 
 }
--- src/providers/get-collection/get-collection.ts
+++ src/providers/get-collection/get-collection.ts
@@ -60,7 +60,6 @@ export class GetCollectionProvider {
                 return false;
             }
         });
-
     }
 
 }
